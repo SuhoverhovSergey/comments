f596446b39e05c1176547b0b15f24c8415f0b50c
Sergey Suhoverhov <geekboox@gmail.com>
2017-04-11 11:03:46 +0300

    MVC skeleton

---
+++ app/views/home/index.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @var array $data
+ */
+?>
+home/index
---
+++ routing.php
@@ -0,0 +1,15 @@
+<?php
+
+$filePath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$file = __DIR__ . '/' . trim($filePath, '/');
+
+if ($filePath != '/' && file_exists($file)) {
+    return false; // файл (или директория) существует, отдаем как есть
+}
+
+$_GET['url'] = $filePath;
+
+unset($filePath, $file);
+
+require_once(__DIR__ . '/public/index.php');
